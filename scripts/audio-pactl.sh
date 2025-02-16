4b5e47338f7c89d8fc8c67a43e161a49aadc5897
davlord <vlord@free.fr>
2025-02-16 19:11:40 +0100

    Audio pactl mark active output

--- scripts/audio-pactl.sh
+++ scripts/audio-pactl.sh
@@ -13,10 +13,12 @@ LANG="en_US.utf8" # force english to avoid translated output
 if [[ $ROFI_RETV = 0 ]]
 then
     echo -en "\0prompt\x1fAudio output\n"
+    readonly default_sink=$(pactl get-default-sink)
     pactl list sinks \
      | grep -E "Name|Description" \
      | awk '{printf "%s ", $0} NR % 2 == 0 {print ""}' \
-     | sed -r "s/Name: ([^ ]+)[ \t]+Description: (.*)/\2\x00info\x1f\1/"
+     | sed -r "s/Name: ([^ ]+)[ \t]+Description: (.*)/\1 \2/" \
+     | awk -v default_sink="$default_sink" '{sink_name=$1; sink_active = sink_name == default_sink ? "true" : "false"; $1=""; sink_description=$0; printf "%s\x00info\x1f%s\x1factive\x1f%s\n", sink_description, sink_name, sink_active}'
 fi
 
 # on selection
