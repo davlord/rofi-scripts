fbf8f8ccfb9b23c09f95f282e15ea6a25a667046
davlord <vlord@free.fr>
2025-02-18 23:34:41 +0100

    Tmuxp

---
+++ scripts/tmuxp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# Select tmuxp project
+# Requires tmuxp to be installed (obviously)
+#
+
+set -euo pipefail
+
+
+# generate list
+if [[ $ROFI_RETV = 0 ]]
+then
+    tmuxp ls
+fi
+
+# on selection
+if [[ $ROFI_RETV = 1 ]] && [ -n "$1" ]
+then
+   coproc ( alacritty --class tmux-project -T tmux:$1 -e tmuxp load $1 )
+fi
