6fbaab32eb77ddfea5d0622c708a5dd15b5cb8f7
davlord <tepes@tuta.io>
2025-02-23 23:43:33 +0100

    VPN nmcli WIP

---
+++ scripts/vpn-nmcli.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# Activate/deactivate VPN connections from networkmanager
+# Requires nmcli to be installed
+#
+
+set -euo pipefail
+
+
+# generate list
+if [[ $ROFI_RETV = 0 ]]
+then
+    nmcli --terse con | awk -F  ':' '{if ($3 == "vpn") { vpn_name=$1; vpn_id=$2; vpn_active = $4 == "" ? "false" : "true"; printf "%s\x00info\x1f%s\x1factive\x1f%s\n", vpn_name, vpn_id, vpn_active } }'
+fi
+
+# on selection
+if [[ $ROFI_RETV = 1 ]] && [ -n "$ROFI_INFO" ]
+then
+    coproc( nmcli connection up "$ROFI_INFO" )
+fi
