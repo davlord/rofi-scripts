38cfe25a0d1091f4b64d89dc650edf9b2ad59c42
davlord <vlord@free.fr>
2025-02-25 23:23:26 +0100

    Bookmarks WIP

--- scripts/web-bookmarks-chrome.sh
+++ scripts/web-bookmarks-chrome.sh
@@ -11,16 +11,16 @@ set -euo pipefail
 declare -A BOOKMARK_FILES 
 BOOKMARK_FILES=(
   ["chromium"]="$HOME/.config/chromium/Default/Bookmarks"
-  ["brave"]=""
+  ["brave"]="$HOME/.config/BraveSoftware/Brave-Browser/Default/Bookmarks"
 )
-BROWSER="chromium"
+BROWSER="brave"
 
 # generate list
 if [[ $ROFI_RETV = 0 ]]
 then
   echo -en "\0prompt\x1fBookmarks from ${BROWSER}\n"
   cat ${BOOKMARK_FILES[${BROWSER}]} | \
-    jq --ascii-output 'def build_path($path):
+    jq --raw-output 'def build_path($path):
 	if .children then
 	    .children[] | build_path($path + "/" + .name)
 	elif has("url") then
@@ -34,6 +34,5 @@ fi
 # on selection
 if [[ $ROFI_RETV = 1 ]] && [ -n "$ROFI_INFO" ]
 then
-    echo "$ROFI_INFO"
-    # xdg-open "$url" > /dev/null 2>&1 & # brave cannot be launched in coproc
+    xdg-open "$ROFI_INFO" > /dev/null 2>&1 & # brave cannot be launched in coproc
 fi
