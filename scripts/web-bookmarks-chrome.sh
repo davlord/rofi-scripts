06c13acf78aac0172cd625f665df9befe2e5fb9a
davlord <vlord@free.fr>
2025-02-25 23:48:15 +0100

    Bookmarks WIP

--- scripts/web-bookmarks-chrome.sh
+++ scripts/web-bookmarks-chrome.sh
@@ -26,7 +26,7 @@ then
 	elif .type == "url" then
 	    $parent + "/" + .name + "\u0000info\u001f" + .url
 	end;
-	.roots.bookmark_bar | .. | objects | select(has("type")) | build_path("")' 
+	.roots.bookmark_bar | build_path("")' 
 fi
 
 # on selection
