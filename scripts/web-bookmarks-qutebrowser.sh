f0ec91c98e30a74362f1d16896ca2669051faa20
davlord <vlord@free.fr>
2025-12-08 22:54:34 +0100

    Add web bookmark script for qutebrowser

---
+++ scripts/web-bookmarks-qutebrowser.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# Select and open from browser bookmarks
+# For chrome based browsers (chromium, brave, ...)
+# Requires: xdg-open, jq
+#
+
+set -euo pipefail
+
+BOOKMARK_FILE="$HOME/.config/qutebrowser/bookmarks/urls"
+
+# generate list
+if [[ $ROFI_RETV = 0 ]]
+then
+  echo -en "\0prompt\x1fBookmarks from qutebrowser\n"
+  awk '{ printf "%s\x00info\x1f%s\n", $2, $1 }' $BOOKMARK_FILE 
+fi
+
+# on selection
+if [[ $ROFI_RETV = 1 ]] && [ -n "$ROFI_INFO" ]
+then
+    xdg-open "$ROFI_INFO" > /dev/null 2>&1 &
+fi
