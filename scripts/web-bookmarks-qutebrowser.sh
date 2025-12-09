904fafc5e925e682d8456b293fb585c4dc2df72a
davlord <tepes@tuta.io>
2025-12-09 09:53:58 +0100

    Add quickmarks in qute

--- scripts/web-bookmarks-qutebrowser.sh
+++ scripts/web-bookmarks-qutebrowser.sh
@@ -8,12 +8,14 @@
 
 set -euo pipefail
 
+QUICKMARK_FILE="$HOME/.config/qutebrowser/quickmarks"
 BOOKMARK_FILE="$HOME/.config/qutebrowser/bookmarks/urls"
 
 # generate list
 if [[ $ROFI_RETV = 0 ]]
 then
   echo -en "\0prompt\x1fBookmarks from qutebrowser\n"
+  awk '{ printf "%s\x00info\x1f%s\n", $1, $2 }' $QUICKMARK_FILE
   awk '{ printf "%s\x00info\x1f%s\n", $2, $1 }' $BOOKMARK_FILE 
 fi
 
